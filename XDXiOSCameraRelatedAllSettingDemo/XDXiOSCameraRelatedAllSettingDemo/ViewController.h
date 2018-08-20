e8f13f974682196ea60dd33b1d96b09a88803f26
Demon <demonli@tvunetworks.com>
2018-08-20 11:19:19 +0800

    Init Git

---
+++ XDXiOSCameraRelatedAllSettingDemo/XDXiOSCameraRelatedAllSettingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDXiOSCameraRelatedAllSettingDemo
+//
+//  Created by 小东邪 on 2018/8/20.
+//  Copyright © 2018 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XDXiOSCameraRelatedAllSettingDemo/XDXiOSCameraRelatedAllSettingDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XDXiOSCameraRelatedAllSettingDemo
+//
+//  Created by 小东邪 on 2018/8/20.
+//  Copyright © 2018 小东邪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
